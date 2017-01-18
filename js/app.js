4426fb5187fcdefd65b0639c68a3ea17ee38fb5e
Tobias Murphy <murphy.tobias@gmail.com>
2017-01-17 17:02:01 -0800

    add function works

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 console.log('app.js was loaded.');
+
+angular.module('App', [])
+.controller('appCtrl', ['$scope', function($scope){
+$scope.display = 0;
+$scope.calc = [];
+
+$scope.clear = function(){
+  $scope.display = 0;
+};
+
+$scope.input = function(num){
+  if ($scope.display === 0){
+    $scope.display = num;
+  } else{
+    $scope.display = $scope.display.toString() + num.toString();
+    $scope.display = parseInt($scope.display, 10);
+  }
+};
+
+$scope.add = function(){
+  $scope.calc.push($scope.display);
+  $scope.display = 0;
+};
+
+$scope.total = function(){
+  $scope.calc.push($scope.display);
+  $scope.display = $scope.calc[0] + $scope.calc[1];
+  $scope.calc = [];
+
+};
+
+}]);
